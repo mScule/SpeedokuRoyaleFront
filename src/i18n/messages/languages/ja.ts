2a4ec3a8f5ec3ffcc54df963467213375655b35f
Vilhelm Niemi <54552657+mScule@users.noreply.github.com>
2022-12-11 18:24:55 +0200

    Julia's Translations for Japanese added

--- src/i18n/messages/languages/ja.ts
+++ src/i18n/messages/languages/ja.ts
@@ -2,28 +2,29 @@ import Messages from "@/i18n/types/message"
 
 const ja: Messages = {
 	// Speedoku royale
-	"speedoku-royale": "Speedoku Royale",
+	"speedoku-royale": "スピードク　ロワイヤル",
 
 	// About
-	"title-about": "JA title-about",
+	"title-about": "ゲームについて",
 
-	"title-speedoku-royale": "JA title-speedoku-royale",
-	"article-speedoku-royale": "JA article-speedoku-royale",
+	"title-speedoku-royale": "スピードク　ロワイヤル　ってどんなゲーム？",
+	"article-speedoku-royale":
+		"スピードク　ロワイヤル、新しいオンラインパズルゲーム。お気に入りのゲームモードを選択することができます。オンラインマルチプレイヤーとしての機会は非常に面白いです。指定時間内には、最も高い得点に達すると勝利します。シングルプレイヤーモードは時間制限のクラシックなナンプレです。",
 
-	"title-how-to-play": "JA title-how-to-play",
-	"article-how-to-play": "JA article-how-to-play",
+	"title-how-to-play": "遊び方？",
+	"article-how-to-play": "これは、ゲームをプレイする方法です…",
 
 	// Play
-	"title-play": "JA title-play",
-	"loading": "JA loading",
+	"title-play": "プレイ",
+	"loading": "読み込み中",
 
 	// Credits
-	"title-credits": "JA title-credits",
+	"title-credits": "作者情報",
 
 	// Page not found
-	"title-not-found": "JA title-not-found",
-	"article-page-not-found": "JA article-page-not-found",
-	"link-go-home": "JA title-go-home"
+	"title-not-found": "ページが見つかりません…",
+	"article-page-not-found": "お探しのページは見つかりませんでした。",
+	"link-go-home": "ホームページに戻る"
 }
 
 export default ja
