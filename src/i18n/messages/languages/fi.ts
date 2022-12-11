614fc8aac1d99748d02ccc14ca5309f16a827492
Vilhelm Niemi <54552657+mScule@users.noreply.github.com>
2022-12-12 01:00:21 +0200

    Temporarily adding the built game into the project folder

--- src/i18n/messages/languages/fi.ts
+++ src/i18n/messages/languages/fi.ts
@@ -8,13 +8,35 @@ const fi: Messages = {
 	"title-about": "Tietoja pelistä",
 
 	"title-speedoku-royale": "Mikä on Speedoku Royale?",
-	"article-speedoku-royale": "Speedoku Royale on 'Battle Royale' -tyylinen peli, jossa tavoitteena on ratkaista sudokuja reaaliajassa toisia pelaajia vastaan kilpailuhenkisesti. Peli sisältää myös yksinpelimuodon, jossa tavoitteena on ratkaista sudokuja aikarajaa vastaan. Moninpeli on kilpailullinen pelimuoto, jossa kunkin pelisession alussa on tietyn verran pelaajia. Pelin edetessä, ratkaistavat sudokut muuttuvat vaikeammiksi ja pelaajia pudotetaan tietyin aikavälein, riippuen kuka on kerännyt vähiten pisteitä tehdyistä sudokuista. Viimeisenä jäljellä oleva, eniten pisteitä kerännyt pelaaja julistetaan voittajaksi. Pelataksesi moninpeliä, sinun tulee olla kirjautuneena sisään.",
+
+	"article-speedoku-royale":
+		"Speedoku Royale on 'Battle Royale' -tyylinen peli, jossa tavoitteena on " +
+		"ratkaista sudokuja reaaliajassa toisia pelaajia vastaan " + 
+		"kilpailuhenkisesti. Peli sisältää myös yksinpelimuodon, jossa " +
+		"tavoitteena on ratkaista sudokuja aikarajaa vastaan. Moninpeli on " +
+		"kilpailullinen pelimuoto, jossa kunkin pelisession alussa on tietyn " +
+		"verran pelaajia. Pelin edetessä, ratkaistavat sudokut muuttuvat " +
+		"vaikeammiksi ja pelaajia pudotetaan tietyin aikavälein, riippuen kuka " +
+		"on kerännyt vähiten pisteitä tehdyistä sudokuista. Viimeisenä jäljellä " +
+		"oleva, eniten pisteitä kerännyt pelaaja julistetaan voittajaksi. " +
+		"Pelataksesi moninpeliä, sinun tulee olla kirjautuneena sisään.",
 
 	"title-how-to-play": "Miten pelata?",
-	"article-how-to-play": "1. Kun olet valinnut haluamasi pelimuodon ja olet valmis pelaamaan, paina 'Click to Start' -nappia. 2. Näkyviin tulee sudoku-ruudukko, jossa on tyhjiä- sekä valmiiksi täytettyjä ruutuja. Täyttääksesi ruudun, liikuta hiirtä haluamallesi ruudun kohdalle ja klikkaa siitä, jolloin ruutu muuttuu aktiiviseksi. Tämän jälkeen voit valita numeron ruudun alaosan numeropalkista tai painaa haluttua numeroa näppäimistöstä. Kun olet klikannut tyhjää ruutua kerran, voit myös liikkua ruudusta toiseen nuolinäppäimen avulla. Voit muuttaa tyhjän ruudun aktiiviseksi painamalla 'Enter' -näppäintä. ",
+
+	"article-how-to-play":
+		"1. Kun olet valinnut haluamasi pelimuodon ja olet " +
+		"valmis pelaamaan, paina 'Click to Start' -nappia. 2. Näkyviin tulee " +
+		"sudoku-ruudukko, jossa on tyhjiä- sekä valmiiksi täytettyjä ruutuja. " +
+		"Täyttääksesi ruudun, liikuta hiirtä haluamallesi ruudun kohdalle ja " +
+		"klikkaa siitä, jolloin ruutu muuttuu aktiiviseksi. Tämän jälkeen voit " +
+		"valita numeron ruudun alaosan numeropalkista tai painaa haluttua " +
+		"numeroa näppäimistöstä. Kun olet klikannut tyhjää ruutua kerran, voit " +
+		"myös liikkua ruudusta toiseen nuolinäppäimen avulla. Voit muuttaa " +
+		"tyhjän ruudun aktiiviseksi painamalla 'Enter' -näppäintä.",
 
 	// Play
 	"title-play": "Pelaa",
+
 	"loading": "Ladataan",
 
 	// Credits
@@ -22,7 +44,9 @@ const fi: Messages = {
 
 	// Page not found
 	"title-not-found": "Ohhoh...",
+
 	"article-page-not-found": "Sivua jolle yritit päästä ei ole olemassa.",
+
 	"link-go-home": "Kotiin"
 }
 
