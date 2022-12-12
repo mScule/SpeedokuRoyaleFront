6d8c307174f323b926c138858559bc6896c910d4
Vilhelm Niemi <54552657+mScule@users.noreply.github.com>
2022-12-12 20:19:37 +0200

    React-unity-webgl go bye bye

--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -1,37 +1,17 @@
 import {FC} from "react"
 import {Navigateable} from "@/components/navigateable"
 import {useTranslate} from "@/i18n"
-import {Unity, useUnityContext} from "react-unity-webgl"
-
-console.log("PATH TO LOADER:", import.meta.env.VITE_UNITY_LOADER_URL)
+import {classes} from "@/design/utils/classes"
+import "./style.css"
 
 export const Play: FC = () => {
 	const translate = useTranslate()
-	const {unityProvider, loadingProgression, isLoaded} = useUnityContext({
-		loaderUrl:    import.meta.env.VITE_UNITY_LOADER_URL,
-		dataUrl:      import.meta.env.VITE_UNITY_DATA_URL,
-		frameworkUrl: import.meta.env.VITE_UNITY_FRAMEWORK_URL,
-		codeUrl:      import.meta.env.VITE_UNITY_CODE_URL
-	})
+
 	return (
 		<Navigateable title={translate("title-play")}>
-			{!isLoaded && (
-				<p>
-					{
-						`${translate("loading")} ... ` +
-						`${Math.round(loadingProgression * 100)}%`
-					}
-				</p>
-			)}
-			<Unity
-				unityProvider={unityProvider}
-				style={{
-					visibility: isLoaded ? "visible" : "hidden",
-					width:  "100%",
-					height: "auto",
-					borderRadius: "var(--roundness-small)"
-				}}
-			/>
+			<iframe
+				className="speedoku-royale"
+				src="http://10.114.32.14/SpeedokuRoyaleWebGL/index.html"></iframe>
 		</Navigateable>
 	)
 }
